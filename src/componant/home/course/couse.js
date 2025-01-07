e238bee813e275a718edf7823c4c9450c86ef354
Akash Gaydhane <117028938+akashgayd@users.noreply.github.com>
2025-01-07 17:57:47 +0530

    chneges

--- src/componant/home/course/couse.js
+++ src/componant/home/course/couse.js
@@ -1,87 +1,58 @@
+import React from "react";
+import "./couse.css";
+import Women from "../../../aseets/run-women-png.png";
+import Lotus from "../../../aseets/lotus-png.webp";
+import Man from "../../../aseets/man-png.png";
+import { FaArrowRight } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
-import Women from '../../../aseets/run-women-png.png';
-import Lotus from '../../../aseets/lotus-png.webp';
-import Man from '../../../aseets/man-png.png';
-import './couse.css';
-
-const Course = ()=>{
-
-
-
-    return(
-       
-
-        <div className='main-course-cont'>
-
-
-<div className='course-1'>
-
-
-<div className='girl-run'>
-<img src={Women}/> 
-
-</div>
-<div className='course-dis'>
-
-<h1> Women Course</h1>
-
-<p>This is for informational purposes only. For medical advice or diagnosis, consult a professional. Generative AI is experimental.</p>
-
-</div>
-
-
-</div>
-
-
-
-
-<div className='course-1'>
-
-<div className='lotus'>
-<img src={Lotus}/>
-</div>
-
-<div className='course-dis'>
-
-
-
-<h1> Ask Anythink...</h1>
-
-<p>This is for informational purposes only. For medical advice or diagnosis, consult a professional. Generative AI is experimental.</p>
-
-
-
-</div>
-
-
-</div>
-
-<div className='course-1'>
-
-
-<div className='man-using-lap'>
-
-<img src={Man}/>
-
-
-</div>
-<div className='course-dis'>
-
-
-
-<h1>Men's Course</h1>
-
-<p>This is for informational purposes only. For medical advice or diagnosis, consult a professional. Generative AI is experimental.</p>
-
-
-
-</div>
-
-</div>
-
+const MainCourse = () => {
+    const navigate = useNavigate();
+    function ask() {
+        navigate("/askNutras");
+      }
+  return (
+    <div className="Main-course-Cont">
+      <div className="main-course-card-cont">
+        <div className="main-course-cards">
+          <img src={Women} className="course-card-images" />
+          <br />
+          <p className="course-card-heading">Women's Fitness</p>
+          <br />
+          <p className="course-card-para">
+            This is for informational purposes only. For medical advice or
+            diagnosis, consult a professional. Generative AI is experimental.
+          </p>
+          <br/>
+          <button className="course-card-btn" onClick={ask}>Ask Nutras <FaArrowRight></FaArrowRight></button>
         </div>
+        <div className="main-course-cards">
+          <img src={Lotus} className="course-card-images" />
+          <br />
+          <p className="course-card-heading"> Ask Anythink...</p>
+          <br />
+          <p className="course-card-para">
+            This is for informational purposes only. For medical advice or
+            diagnosis, consult a professional. Generative AI is experimental.
+          </p>
+          <br />
+          <button className="course-card-btn" onClick={ask}>Ask Nutras <FaArrowRight></FaArrowRight></button>
+        </div>
+        <div className="main-course-cards">
+          <img src={Man} className="course-card-images" />
+          <br />
+          <p className="course-card-heading">Men's Fitness</p>
+          <br />
+          <p className="course-card-para">
+            This is for informational purposes only. For medical advice or
+            diagnosis, consult a professional. Generative AI is experimental.
+          </p>
+          <br/>
+          <button className="course-card-btn" onClick={ask}>Ask Nutras <FaArrowRight></FaArrowRight></button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-
-}
-export  default Course;
+export default MainCourse;
