ea1f5b30e725887749edfd230a41f3dd436ba3ec
Akash Gaydhane <117028938+akashgayd@users.noreply.github.com>
2025-01-09 13:18:00 +0530

    new

--- src/componant/common/slider/textSlider.js
+++ src/componant/common/slider/textSlider.js
@@ -8,9 +8,7 @@ const TextSlider = () => {
     
       <Marquee play={true} className="marquee" speed={70} >
         <div className="main-marque-cont">
-        <div className="text2">
-        "Limited-Time Offer: Get 30% Off on All Fitness Plans
-        </div>
+
         <div className="text1">
         Transform Your Fitness Journey Today!
         </div>
@@ -18,7 +16,7 @@ const TextSlider = () => {
         Exclusive Fitness Tips and Workouts
         </div>
         <div className="text1">
-        "Limited-Time Offer: Get 30% Off on All Fitness Plans
+        Limited-Time Offer: Get 30% Off on All Fitness Plans
         </div>
        
        
